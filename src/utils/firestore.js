54c75b94fa3d964897985e2006434a81f445f711
NeftisDragon <gregocm1000@gmail.com>
2022-07-04 19:15:58 -0300

    #

---
+++ src/utils/firestore.js
@@ -0,0 +1,17 @@
+//cSpell: disable
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBaDzWrSIxylHomGeQrkJfz3cXPpmh2ilU",
+    authDomain: "frostwing-e2279.firebaseapp.com",
+    projectId: "frostwing-e2279",
+    storageBucket: "frostwing-e2279.appspot.com",
+    messagingSenderId: "184254815854",
+    appId: "1:184254815854:web:78d7b76fb3f99111ba107f"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
